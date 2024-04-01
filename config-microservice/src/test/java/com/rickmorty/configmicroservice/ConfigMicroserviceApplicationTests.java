6623b66f0da6fa4a7ab5ed70222f1e273caa1583
Extremera <extremera10extremera@gmail.com>
2024-04-01 11:55:50 +0200

    Subiendo modificaciones con config server

---
+++ config-microservice/src/test/java/com/rickmorty/configmicroservice/ConfigMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rickmorty.configmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
