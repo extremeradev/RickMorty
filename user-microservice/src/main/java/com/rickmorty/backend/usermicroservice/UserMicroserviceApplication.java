46a32b11b38b5b0491bf4c752f8fe8786080f32d
Extremera <extremera10extremera@gmail.com>
2024-04-01 12:13:51 +0200

    Añadiendo eureka

--- user-microservice/src/main/java/com/rickmorty/backend/usermicroservice/UserMicroserviceApplication.java
+++ user-microservice/src/main/java/com/rickmorty/backend/usermicroservice/UserMicroserviceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserMicroserviceApplication {
 
 	public static void main(String[] args) {
