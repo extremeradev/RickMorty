5ba7f486f7175c7994112e29b05887584749ddd3
Extremera <extremera10extremera@gmail.com>
2024-04-04 18:32:41 +0200

    Añadiendo gateway

--- user-microservice/src/main/java/com/rickmorty/backend/usermicroservice/feignclients/FavFeignClient.java
+++ user-microservice/src/main/java/com/rickmorty/backend/usermicroservice/feignclients/FavFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "favoritos-microservice")
+@FeignClient(name = "FAVORITOS-MICROSERVICE")
 public interface FavFeignClient {
 
     @PostMapping()
